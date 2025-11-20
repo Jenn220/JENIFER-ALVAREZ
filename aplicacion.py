f351bce885b99b79257775c4bdf32c26864a81b0
unknown <jennialvarez114@gmail.com>
2025-11-20 14:05:27 -0500

    token

---
+++ aplicacion.py
@@ -0,0 +1,30 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+    <h1>Hola! Aplicación de Jenifer :3 :3 :3</h1>
+    <p>Esta es mi aplicación para la tarea de 14 de Noviembre</p>
+    <a href="/suma/5/3">Probar suma: 5 + 3</a><br>
+    <a href="/resta/10/4">Probar resta: 10 - 4</a>
+    '''
+
+@app.route('/suma/<path:a>/<path:b>')
+def sumar(a, b):
+    a = int(a)
+    b = int(b)
+    resultado = a + b
+    return f'<h2>Resultado de {a} + {b} = {resultado}</h2>'
+
+@app.route('/resta/<path:a>/<path:b>')
+def restar(a, b):
+    a = int(a)
+    b = int(b)
+    resultado = a - b
+    return f'<h2>Resultado de {a} - {b} = {resultado}</h2>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=False)
